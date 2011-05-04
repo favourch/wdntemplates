39f6bec79989869c46209cb566352c0979f41bc2
Seth Meranda <smeranda2@unl.edu>
2011-05-04 21:41:34 +0000

    fix class name for mega box (mega, not megabox).

--- wdn/templates_3.0/scripts/navigation.js
+++ wdn/templates_3.0/scripts/navigation.js
@@ -78,7 +78,7 @@ WDN.navigation = function() {
                     interval:    120
                 });
                 */
-                WDN.jQuery('#navigation.megabox > ul').addClass('nav_collapsed').hoverIntent({
+                WDN.jQuery('#navigation.mega > ul').addClass('nav_collapsed').hoverIntent({
                     over:        WDN.navigation.showFullNavigation,
                     out:         WDN.navigation.hideFullNavigation,
                     timeout:     WDN.navigation.changeSiteNavDelay,
