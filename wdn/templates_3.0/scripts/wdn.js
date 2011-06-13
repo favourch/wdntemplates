e94612d2959ef9d9703b94a860768bf107d14d98
Brett Bieber <brett.bieber@gmail.com>
2011-06-13 21:53:09 +0000

    Make the border color consistent with what is in CSS

--- wdn/templates_3.0/scripts/wdn.js
+++ wdn/templates_3.0/scripts/wdn.js
@@ -193,7 +193,7 @@ var WDN = function() {
 			WDN.jQuery('#footer_floater').css("zoom", 1);
 			WDN.jQuery('#maincontent p.caption, #footer p.caption').each(function(i){
 				if (WDN.jQuery(this).height()>20) {
-					WDN.jQuery(this).css({border:'1px solid #ededed',marginleft:'0'});
+					WDN.jQuery(this).css({border:'1px solid #DDD',marginleft:'0'});
 				}
 				//set the caption to the same width as the image it goes with so that a long caption doesn't spill over
 				var imgWidth = WDN.jQuery(this).prev('img').width();
