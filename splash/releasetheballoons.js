b0656d5d3f14018d00dbbd1f5383e2efb13c173c
Eric Rasmussen <erasmussen2@unl.edu>
2011-06-24 14:22:47 +0000

    IE Fixes

--- splash/releasetheballoons.js
+++ splash/releasetheballoons.js
@@ -1,21 +1,24 @@
 var UNL_Balloons = (function() {
 	var $; // Will set to our jQuery variable once it's available on the callback so we can use $
 	
-	var balloonBatch = 10; // Number of balloons to release each time
+	var balloonBatch = 8; // Number of balloons to release each time
 	var maxBalloons = 55; // Max number of balloons before balloon overdose
 	var clickActive = true; // Set to false when maxBalloons has been reached
 	var balloonRegistry = new Array(); // Keep track of what balloons are currently on screen
 	
 	var interval = new Object(); // setInterval variable for interval.move and interval.hadEnough
-	var moveTimeSpan = 30; // Time span for move interval
+	var moveTimeSpan = 25; // Time span for move interval
 	var moveDistance = 1; // Pixel distance to move
 	
 	return {
 		init : function() {
 			$ = WDN.jQuery;
 			
+			// Throttle it for IE 8
+			if ($.browser.msie && $.browser.version < 9.0 ) {
+				maxBalloons = 20;
+			}
 			
-
 			$('#footer').prepend('<div id="releasetheballoons"><img id="releasetheballoons-img" src="splash/releasetheballoons.png" alt="Click to Release the Balloons" /></div>');
 			
 			$('#releasetheballoons-img').click(function() {
@@ -30,7 +33,7 @@ var UNL_Balloons = (function() {
 					interval.hadEnough = setInterval('UNL_Balloons.checkMaxBalloons();', 1000);
 				} else {
 					UNL_Balloons.addBalloons();
-					WDN.log(balloonRegistry);
+					WDN.log('balloonRegistry: '+balloonRegistry);
 					if (interval.move == null) {
 						WDN.log('Release the balloons!');
 						UNL_Balloons.startBalloons();
@@ -47,8 +50,13 @@ var UNL_Balloons = (function() {
 		addBalloons : function() {
 			var lastIndex = (balloonRegistry.length ? balloonRegistry.length-1 : 0);
 			var lastBalloon = (balloonRegistry[lastIndex] ? balloonRegistry[lastIndex] : 0);
-
-			var centerPosition = (window.innerWidth/2)-35;
+			
+			var centerPosition;
+			if ($.browser.msie && $.browser.version < 9.0) {
+				centerPosition = (document.body.clientWidth/2)-35;
+			} else {
+				centerPosition = (window.innerWidth/2)-35;
+			}
 			var footerPosition = $('#footer').offset().top;
 			
 			var i;
@@ -77,9 +85,8 @@ var UNL_Balloons = (function() {
 				var currentBalloon = $('#balloon'+balloonRegistry[i]);
 				var offset = currentBalloon.offset();
 
-				// Get the last digit of the balloon id, i.e. 9 for balloon59
-				var sigFig = balloonRegistry[i].toString().substr(-1);
-				sigFig = parseInt(sigFig);
+				// Get the last digit of the balloon id, i.e. 9 for 59
+				var sigFig = balloonRegistry[i]%10;
 			
 				// Do some stuff with random numbers to try to get some different flow amongst balloons
 				// Basically this was the result of trial and error,
@@ -99,7 +106,7 @@ var UNL_Balloons = (function() {
 				// If the balloon has exited the top of the view port, remove from the DOM and the registry
 				if (offset.top < -80) {
 					currentBalloon.remove();
-					WDN.log('Removed balloon key: '+i+' Balloon id:'+currentBalloon.attr('id'));
+					WDN.log('Removed balloon key: '+i+'; Balloon id: '+currentBalloon.attr('id'));
 					balloonRegistry.splice(i, 1);
 					WDN.log(' Registry is now: '+balloonRegistry);
 				}
