5a7a8b50c5a6330d9e47075404954d13ba16edb6
Eric Rasmussen <erasmussen2@unl.edu>
2011-06-28 21:07:35 +0000

    Use css background for the balloons to placate Kevin

--- splash/releasetheballoons.js
+++ splash/releasetheballoons.js
@@ -1,26 +1,26 @@
 var UNL_Balloons = (function() {
 	var $; // Will set to our jQuery variable once it's available on the callback so we can use $
-	
+
 	var balloonBatch = 8; // Number of balloons to release each time
 	var maxBalloons = 55; // Max number of balloons before balloon overdose
 	var clickActive = true; // Set to false when maxBalloons has been reached
 	var balloonRegistry = new Array(); // Keep track of what balloons are currently on screen
-	
+
 	var interval = new Object(); // setInterval variable for interval.move and interval.hadEnough
 	var moveTimeSpan = 25; // Time span for move interval
 	var moveDistance = 1; // Pixel distance to move
-	
+
 	return {
 		init : function() {
 			$ = WDN.jQuery;
-			
+
 			// Throttle it for IE 8
 			if ($.browser.msie && $.browser.version < 9.0 ) {
 				maxBalloons = 20;
 			}
-			
+
 			$('#footer').prepend('<div id="releasetheballoons"><img id="releasetheballoons-img" src="splash/releasetheballoons.png" alt="Click to Release the Balloons" /></div>');
-			
+
 			$('#releasetheballoons-img').click(function() {
 				if (!clickActive) {
 					return;
@@ -40,17 +40,17 @@ var UNL_Balloons = (function() {
 					}
 				}
 			});
-			
+
 			//Let's just go ahead and release some beloons on page load
 			window.onload = function() {
 				$('#releasetheballoons-img').click();
 			};
 		},
-		
+
 		addBalloons : function() {
 			var lastIndex = (balloonRegistry.length ? balloonRegistry.length-1 : 0);
 			var lastBalloon = (balloonRegistry[lastIndex] ? balloonRegistry[lastIndex] : 0);
-			
+
 			var centerPosition;
 			if ($.browser.msie && $.browser.version < 9.0) {
 				centerPosition = (document.body.clientWidth/2)-35;
@@ -58,20 +58,20 @@ var UNL_Balloons = (function() {
 				centerPosition = (window.innerWidth/2)-35;
 			}
 			var footerPosition = $('#footer').offset().top;
-			
+
 			var i;
 			for (i=lastBalloon+1;i<=lastBalloon+balloonBatch;i++) {
-				$('body').append('<img class="balloon" id="balloon'+i+'" src="splash/balloon.png" alt="Red Balloon" />');
+				$('body').append('<div class="balloon" id="balloon'+i+'">BALLOON FLOATING ACROSS THE SCREEN!</div>');
 				$('#balloon'+i).css({'left' : centerPosition+'px', 'top' : footerPosition+'px'});
 				WDN.log('Added balloon'+i);
 				balloonRegistry.push(i);
 			}
 		},
-		
+
 		startBalloons : function() {
 			interval.move = setInterval('UNL_Balloons.moveBalloons();', moveTimeSpan);
 		},
-		
+
 		moveBalloons : function() {
 			if (balloonRegistry.length < 1) {
 				clearInterval(interval.move);
@@ -87,7 +87,7 @@ var UNL_Balloons = (function() {
 
 				// Get the last digit of the balloon id, i.e. 9 for 59
 				var sigFig = balloonRegistry[i]%10;
-			
+
 				// Do some stuff with random numbers to try to get some different flow amongst balloons
 				// Basically this was the result of trial and error,
 				// Not ideal but I'm not going to write a whole damn balloon physics engine (like I'd know how anyway)
@@ -102,7 +102,7 @@ var UNL_Balloons = (function() {
 
 				// Actually move the balloon
 				currentBalloon.css({'left' : offset.left+moveHorizontal+'px', 'top' : offset.top-moveVertical+'px'});
-				
+
 				// If the balloon has exited the top of the view port, remove from the DOM and the registry
 				if (offset.top < -80) {
 					currentBalloon.remove();
